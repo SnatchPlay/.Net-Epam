7c9f99b01d3923b7f75e7da12a292e2e1697e808
Andriy Popovych <snatch44233@gmail.com>
2022-06-26 12:38:13 +0300

    Added answer to tas\k 2

---
+++ Task/2-Answer/LoopTasks/LoopTasks.cs
@@ -0,0 +1,58 @@
+﻿namespace LoopTasks
+{
+    public static class LoopTasks
+    {
+        /// <summary>
+        /// Task 1
+        /// </summary>
+        public static int SumOfOddDigits(int n)
+        {
+            int sum = 0;
+            while (n > 0)
+            {
+                if (n % 2 != 0)
+                {
+                    sum = sum + (n % 10);
+                }
+                n /= 10;
+            }
+            return sum;
+        }
+
+        /// <summary>
+        /// Task 2
+        /// </summary>
+        public static int NumberOfUnitsInBinaryRecord(int n)
+        {
+            int sum = 0;
+            while (n > 0)
+            {
+                sum = sum + (n % 2);
+                n /= 2;
+            }
+            return sum;
+
+
+        }
+
+        /// <summary>
+        /// Task 3 
+        /// </summary>
+        public static int SumOfFirstNFibonacciNumbers(int n)
+        {
+            int c = 0, sum = 1, a = 0, b = 1;
+            for (int i = 2; i < n; i++)
+            {
+                c = a + b;
+                sum += c;
+                a = b;
+                b = c;
+            }
+
+            if (n == 1) { return a; }
+            else { return sum; }
+
+
+        }
+    }
+}
