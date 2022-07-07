8514e6b2097e0837bd88acba4922ea3caec16d0b
Andriy Popovych <snatch44233@gmail.com>
2022-07-07 20:40:10 +0300

    Answer to task 12 9/15

--- Task/12-Linq/Linq/Tasks.cs
+++ Task/12-Linq/Linq/Tasks.cs
@@ -1,7 +1,7 @@
-﻿using System;
+﻿using Linq.Objects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using Linq.Objects;
 
 namespace Linq
 {
@@ -18,23 +18,28 @@ namespace Linq
         public static IEnumerable<string> Task1(char c, IEnumerable<string> stringList)
         {
             return stringList.Select(x => x.ToString())
-                .Where(x=>x.Length>1&&x.StartsWith(c)&&x.EndsWith(c));
+                .Where(x => x.Length > 1 && x.StartsWith(c) && x.EndsWith(c));
         }
 
         public static IEnumerable<int> Task2(IEnumerable<string> stringList)
         {
-            return stringList.Select(x=>x.Length).OrderBy(x=>x);
+            return stringList.Select(x => x.Length).OrderBy(x => x);
 
         }
 
         public static IEnumerable<string> Task3(IEnumerable<string> stringList)
         {
-            return stringList.Select(x => x.First()+x.Last().ToString());
+            return stringList.Select(x => x.First() + x.Last().ToString());
         }
 
         public static IEnumerable<string> Task4(int k, IEnumerable<string> stringList)
         {
-            return stringList.Where(x => x.Length.Equals(k)&&x.SkipLast(1).Any(char.IsNumber)).OrderBy(x=>x.Last());
+            IEnumerable<string> result = stringList.
+                Where(x => x.Length.Equals(k));
+            result = result.Where(x => char.IsDigit(x.Last()));
+            result = result.OrderBy(x => x);
+            return result;
+
 
         }
         /// <summary>
@@ -56,32 +61,76 @@ namespace Linq
 
         public static IEnumerable<string> Task6(IEnumerable<int> numbers, IEnumerable<string> stringList)
         {
-            //TODO :Delete line below and write your own solution 
-            throw new NotImplementedException();
+            int k = 0;
+            IEnumerable<string> strings = Enumerable.Empty<string>();
+            foreach (int n in numbers)
+            {
+
+                foreach (string s in stringList)
+                {
+                    k = 0;
+                    if (s.Length.Equals(n) && char.IsDigit(s.First()))
+                    {
+
+                        strings = strings.Append(s);
+                        k++;
+                        break;
+
+                    }
+                }
+                if (k == 0)
+                {
+                    strings = strings.Append("Not found");
+                }
+            }
+
+
+
+
+            return numbers.Select(n => stringList.
+            FirstOrDefault(y => y.Length == n && y.Length > 0 && char.IsDigit(y[0])) ?? "Not found");
+            //return strings;
+
         }
+        //A sequence of positive integers numbers and a sequence of strings stringList are given.
+        //    Get a new sequence of strings according to the following rule: for each value n from sequence numbers,
+        //    select a string from sequence stringList that starts with a digit and has length n.
+        //    If there are several required strings in the stringList sequence, return the first;
+        //if there are none, then return the string "Not found" 
+        //    (To handle the situation related to the absence of required strings, use the ?? operation)
 
         public static IEnumerable<int> Task7(int k, IEnumerable<int> integerList)
         {
-            //TODO :Delete line below and write your own solution 
-            throw new NotImplementedException();
+            return integerList.Where(x => x % 2 == 0).Except(integerList.Where((x, index) => index > k - 1)).Reverse();
         }
-        
+
         public static IEnumerable<int> Task8(int k, int d, IEnumerable<int> integerList)
         {
-            //TODO :Delete line below and write your own solution 
-            throw new NotImplementedException();
+            return integerList.TakeWhile(x => x <= d).Union(integerList.Skip(k).Take(integerList.Count() - k)).OrderByDescending(x => x);
         }
 
         public static IEnumerable<string> Task9(IEnumerable<string> stringList)
         {
-            //TODO :Delete line below and write your own solution 
             throw new NotImplementedException();
         }
 
         public static IEnumerable<string> Task10(IEnumerable<string> stringList)
         {
-            //TODO :Delete line below and write your own solution 
-            throw new NotImplementedException();
+            IEnumerable<string> result = stringList.OrderBy(x => x);
+            IEnumerable<string> result2 =  Enumerable.Empty<string>();
+            var group= result.GroupBy(x => x.Length);
+            foreach (var s in group)
+            {
+                string ss = string.Empty;
+                foreach (var v in s)
+                {
+                    ss += v.ToUpper().Last();
+                }
+                result2 = result2.Append(ss);
+            }
+            return result2.OrderByDescending(x=>x.Length);
+            
+
         }
 
         #endregion
