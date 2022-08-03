9722a9c92c836a1ba40358c30642a318b13d2f5e
Andriy Popovych <snatch44233@gmail.com>
2022-08-03 15:23:14 +0300

    Added task 13

---
+++ JS Tasks/13-hw-scope-and-context/src/scope-and-context.js
@@ -0,0 +1,10 @@
+function Fighter() {
+  // add an implementation here
+  
+}
+
+const battle = function (fighter1, fighter2) {
+  // add an implementation here
+};
+
+module.exports = { Fighter, battle};
