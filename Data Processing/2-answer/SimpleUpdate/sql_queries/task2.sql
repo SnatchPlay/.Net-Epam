b9919ee651607c6ccc8ae1d0f9a51b50664a9bb6
Andriy Popovych <snatch44233@gmail.com>
2022-08-04 16:20:25 +0300

    Added task 2 and answer

---
+++ Data Processing/2-answer/SimpleUpdate/sql_queries/task2.sql
@@ -0,0 +1,8 @@
+﻿UPDATE product
+   SET price = price-(price*25/100)
+ WHERE product_title_id IN (
+    SELECT id
+      FROM product_title
+     WHERE product_category_id = 1 OR 
+           product_category_id = 3
+);
---
+++ Data Processing/2-answer/SimpleUpdate/sql_queries/task4.sql
@@ -0,0 +1,7 @@
+﻿UPDATE product
+   SET price = price + (price * 30 / 100) 
+ WHERE product_title_id IN (
+    SELECT id
+      FROM product_title
+     WHERE product_category_id = 5
+);
