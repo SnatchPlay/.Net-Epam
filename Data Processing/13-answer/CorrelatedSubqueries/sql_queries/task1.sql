201fb0b123c911b116aa8e9165e627b50138be09
Andriy Popovych <snatch44233@gmail.com>
2022-08-07 14:08:01 +0300

    Added tasks 13 and raw answer

---
+++ Data Processing/13-answer/CorrelatedSubqueries/sql_queries/task1.sql
@@ -0,0 +1,8 @@
+﻿SELECT city.id AS id,
+       city.name AS name
+  FROM city
+ WHERE id NOT IN (
+           SELECT DISTINCT street.city_id
+             FROM street
+       )
+ ORDER BY name;
---
+++ Data Processing/13-answer/CorrelatedSubqueries/sql_queries/task2.sql
@@ -0,0 +1,35 @@
+﻿SELECT product_category.id AS id,
+       product_category.name AS name
+  FROM product_category
+       INNER JOIN
+       (
+           SELECT product_category.id AS subid,
+                  product_category.name AS name
+             FROM product_category
+            WHERE id NOT IN (
+                      SELECT product_title.product_category_id AS title_id
+                        FROM product_title
+                       WHERE product_title.id IN (
+                                 SELECT product.product_title_id AS product_id
+                                   FROM product
+                                  WHERE product.id NOT IN (
+                                            SELECT DISTINCT order_details.product_id
+                                              FROM order_details
+                                        )
+                             )
+                  )
+       )
+       AS subb ON subb.subid = id
+ WHERE id IN (
+           SELECT DISTINCT product_title.product_category_id AS id
+             FROM product_title
+            WHERE product_title.id IN (
+                      SELECT product.product_title_id AS id
+                        FROM product
+                       WHERE product.id IN (
+                                 SELECT DISTINCT order_details.product_id AS id
+                                   FROM order_details
+                             )
+                  )
+       )
+ ORDER BY id;
